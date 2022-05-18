a1460ceabceec29e19de2b4de5c193a491677e87
Atalay Ozkan <matalayath@gmail.com>
2022-05-18 05:57:59 +0300

    This is for Jenkins

---
+++ src/net/bilesim/Main.java
@@ -0,0 +1,13 @@
+package net.bilesim;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("5 + 3 ="+5+3);
+
+
+
+    }
+}
