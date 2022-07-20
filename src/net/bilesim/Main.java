919d10ab6ab23756e5a67a43deaf4c3960960498
Atalay Ozkan <matalayath@gmail.com>
2022-07-20 16:15:36 +0300

    This is for Jenkins

--- src/net/bilesim/Main.java
+++ src/net/bilesim/Main.java
@@ -1,13 +1,13 @@
 package net.bilesim;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
-        System.out.println("5 + 3 ="+5+3);
+        // write your code here
 
-
+        System.out.println("Taluhan Ones- Atalay Ozkan");
 
     }
 }
